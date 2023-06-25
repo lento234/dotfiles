122d780ba20beab30ee7419869729b4c0dea1eea
MrLento234 <lento.manickathan@gmail.com>
2023-06-25 20:52:06 +0200

    added better path

--- nvim/templates/dap-python/.nvim/nvim-dap.lua
+++ nvim/templates/dap-python/.nvim/nvim-dap.lua
@@ -2,8 +2,7 @@ local dap = require('dap')
 
 dap.adapters.python = {
     type = 'executable',
-    command = 'python',
-    args = { '-m', 'debugpy.adapter' },
+    command = vim.fn.stdpath('data') .. '/mason/bin/debugpy-adapter',
     options = {
         source_filetype = 'python',
     }
