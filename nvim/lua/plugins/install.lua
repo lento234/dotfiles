8f5acc4c1c5cee457d30d30125f2bb05ee39b9ee
Lento Manickathan <lento.manickathan@disneyresearch.com>
2023-09-25 08:57:18 +0200

    updated to improved font; added go dev support in nvim

--- nvim/lua/plugins/install.lua
+++ nvim/lua/plugins/install.lua
@@ -175,6 +175,9 @@ return require('packer').startup(function(use)
   -- whitespace
   use 'ntpeters/vim-better-whitespace'
 
+  -- go development
+  use 'fatih/vim-go'
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
