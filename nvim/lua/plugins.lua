ec57c661fac52710c5bd8580c453cda369c2764d
Lento Manickathan <lento.manickathan@gmail.com>
2024-01-23 17:34:45 +0100

    added neogit

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -244,6 +244,17 @@ require("lazy").setup({
             end,
         },
     },
+    {
+        "NeogitOrg/neogit",
+        dependencies = {
+            "nvim-lua/plenary.nvim",         -- required
+            "sindrets/diffview.nvim",        -- optional - Diff integration
+
+            -- Only one of these is needed, not both.
+            "nvim-telescope/telescope.nvim", -- optional
+        },
+        config = true
+    },
 
     -- harpoon
     {
