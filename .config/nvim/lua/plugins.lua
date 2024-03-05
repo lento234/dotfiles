3f2a7e9d94ab9e3dea475f85efd66862a1e66131
Lento Manickathan <lento.manickathan@gmail.com>
2024-03-05 15:35:13 +0100

    added outline viewer, added c,cpp,ts specific rules

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -57,6 +57,15 @@ require("lazy").setup({
             })
         end,
     },
+    {
+        "hedyhli/outline.nvim",
+        lazy = true,
+        cmd = { "Outline", "OutlineOpen" },
+        keys = { -- Example mapping to toggle outline
+            { "<leader>o", "<cmd>Outline<CR>", desc = "Toggle outline" },
+        },
+        opts = {},
+    },
     -- status line
     {
         "nvim-lualine/lualine.nvim",
