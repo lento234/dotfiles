1e342d7658db311c45066393dc05be37f4af5e3a
Lento Manickathan <lento.manickathan@gmail.com>
2024-05-16 09:51:00 +0200

    linted + updated harpoon

--- .config/nvim/lua/custom/plugins/conform.lua
+++ .config/nvim/lua/custom/plugins/conform.lua
@@ -1,26 +1,27 @@
 return {
-    'stevearc/conform.nvim',
-    event = { "BufReadPre", "BufNewFile" },
-    opts = {
-        formatters_by_ft = {
-            -- Conform will run multiple formatters sequentially
-            css = { "prettier" },
-            html = { "prettier" },
-            lua = { "stylua" },
-            python = { "ruff_lsp" },
-            -- Use a sub-list to run only the first available formatter
-            -- javascript = { { "prettierd", "prettier" } },
-            javascript = { "prettier" },
-            typescript = { "prettier" },
-        },
-        format_on_save = function(bufnr)
-            if not vim.g.format_on_save then
-                return
-            end
-            if vim.bo[bufnr].filetype == 'vue' then
-                return { timeout_ms = 500, lsp_fallback = false, formatters = { "prettier" } }
-            end
-            return { timeout_ms = 500, lsp_fallback = true }
-        end,
-    },
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	opts = {
+		formatters_by_ft = {
+			-- Conform will run multiple formatters sequentially
+			css = { "prettier" },
+			cpp = { "clang-format" },
+			html = { "prettier" },
+			lua = { "stylua" },
+			python = { "ruff_lsp" },
+			-- Use a sub-list to run only the first available formatter
+			-- javascript = { { "prettierd", "prettier" } },
+			javascript = { "prettier" },
+			typescript = { "prettier" },
+		},
+		format_on_save = function(bufnr)
+			if not vim.g.format_on_save then
+				return
+			end
+			if vim.bo[bufnr].filetype == "vue" then
+				return { timeout_ms = 500, lsp_fallback = false, formatters = { "prettier" } }
+			end
+			return { timeout_ms = 500, lsp_fallback = true }
+		end,
+	},
 }
--- .config/nvim/lua/custom/plugins/harpoon.lua
+++ .config/nvim/lua/custom/plugins/harpoon.lua
@@ -1,28 +1,33 @@
 return {
-  "ThePrimeagen/harpoon",
-  branch = "harpoon2",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    require("harpoon").setup({
-      settings = {
-        -- sets the marks upon calling `toggle` on the ui, instead of require `:w`.
-        save_on_toggle = true,
-      }
-    })
-    local function noremap(mode, lhs, rhs, desc, opts)
-      opts = opts or { silent = true }
-      vim.keymap.set(mode, lhs, rhs, { desc = desc, noremap = true, silent = opts["silent"] })
-    end
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		require("harpoon").setup({
+			settings = {
+				-- sets the marks upon calling `toggle` on the ui, instead of require `:w`.
+				save_on_toggle = true,
+			},
+		})
+		local function noremap(mode, lhs, rhs, desc, opts)
+			opts = opts or { silent = true }
+			vim.keymap.set(mode, lhs, rhs, { desc = desc, noremap = true, silent = opts["silent"] })
+		end
 
-    -- plugin: harpoon
-    local harpoon = require("harpoon")
+		-- plugin: harpoon
+		local harpoon = require("harpoon")
 
-    noremap("n", "<leader>a", function() harpoon:list():append() end, "harpoon: [a]ppend file")
-    noremap("n", "<leader>e", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
-      "harpoon: open [e]xplore menulist")
+		noremap("n", "<leader>a", function()
+			harpoon:list():add()
+		end, "harpoon: [a]ppend file")
+		noremap("n", "<leader>e", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end, "harpoon: open [e]xplore menulist")
 
-    for i = 1, 9 do
-      noremap("n", "<A-" .. i .. ">", function() harpoon:list():select(i) end, "harpoon: select item [" .. i .. "]")
-    end
-  end,
+		for i = 1, 9 do
+			noremap("n", "<A-" .. i .. ">", function()
+				harpoon:list():select(i)
+			end, "harpoon: select item [" .. i .. "]")
+		end
+	end,
 }
