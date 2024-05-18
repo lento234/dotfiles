d73952c78c2b1c46c0403ab356c4cac2aa894116
Lento Manickathan <lento.manickathan@gmail.com>
2024-05-18 22:04:45 +0200

    linted

--- .config/nvim/lua/custom/plugins/treesitter.lua
+++ .config/nvim/lua/custom/plugins/treesitter.lua
@@ -1,63 +1,63 @@
 return {
-  -- Highlight, edit, and navigate code
-  'nvim-treesitter/nvim-treesitter',
-  event = { "BufReadPre", "BufNewFile" },
-  dependencies = {
-    'nvim-treesitter/nvim-treesitter-textobjects',
-    "windwp/nvim-ts-autotag",
-  },
-  build = ':TSUpdate',
-  config = function()
-    local treesitter = require("nvim-treesitter.configs")
-    treesitter.setup({
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-      indent = { enable = true },
-      autotag = { enable = true },
-      ensure_installed = {
-        'bash',
-        'c',
-        'cpp',
-        'go',
-        'lua',
-        'markdown',
-        'python',
-        'vim',
-        'yaml',
-      },
-      auto_install = true,
+	-- Highlight, edit, and navigate code
+	"nvim-treesitter/nvim-treesitter",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		"windwp/nvim-ts-autotag",
+	},
+	build = ":TSUpdate",
+	config = function()
+		local treesitter = require("nvim-treesitter.configs")
+		treesitter.setup({
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+			indent = { enable = true },
+			autotag = { enable = true },
+			ensure_installed = {
+				"bash",
+				"c",
+				"cpp",
+				"go",
+				"lua",
+				"markdown",
+				"python",
+				"vim",
+				"yaml",
+			},
+			auto_install = true,
 
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = '<c-space>',
-          node_incremental = '<c-space>',
-          scope_incremental = '<c-s>',
-          node_decremental = '<M-space>',
-        },
-      },
-      move = {
-        enable = true,
-        set_jumps = true, -- whether to set jumps in the jumplist
-        goto_next_start = {
-          [']m'] = '@function.outer',
-          [']]'] = '@class.outer',
-        },
-        goto_next_end = {
-          [']M'] = '@function.outer',
-          [']['] = '@class.outer',
-        },
-        goto_previous_start = {
-          ['[m'] = '@function.outer',
-          ['[['] = '@class.outer',
-        },
-        goto_previous_end = {
-          ['[M'] = '@function.outer',
-          ['[]'] = '@class.outer',
-        },
-      },
-    })
-  end,
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<c-space>",
+					node_incremental = "<c-space>",
+					scope_incremental = "<c-s>",
+					node_decremental = "<M-space>",
+				},
+			},
+			move = {
+				enable = true,
+				set_jumps = true, -- whether to set jumps in the jumplist
+				goto_next_start = {
+					["]m"] = "@function.outer",
+					["]]"] = "@class.outer",
+				},
+				goto_next_end = {
+					["]M"] = "@function.outer",
+					["]["] = "@class.outer",
+				},
+				goto_previous_start = {
+					["[m"] = "@function.outer",
+					["[["] = "@class.outer",
+				},
+				goto_previous_end = {
+					["[M"] = "@function.outer",
+					["[]"] = "@class.outer",
+				},
+			},
+		})
+	end,
 }
