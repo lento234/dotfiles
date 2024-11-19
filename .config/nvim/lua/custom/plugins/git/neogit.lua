878631d8412cee55412e93e9a9edbe8bbb79acc7
Lento Manickathan <lento.manickathan@gmail.com>
2024-11-19 17:01:32 +0100

    added kitty style

--- .config/nvim/lua/custom/plugins/git/neogit.lua
+++ .config/nvim/lua/custom/plugins/git/neogit.lua
@@ -7,9 +7,15 @@ return {
 
 		-- Only one of these is needed, not both.
 		"nvim-telescope/telescope.nvim", -- optional
+		"ibhagwan/fzf-lua",            -- optional
+		"echasnovski/mini.pick",       -- optional
 	},
 	keys = {
 		{ "<leader>gg", ":Neogit<CR>", desc = "toggle neo[g]it [g]" },
 	},
-	config = true,
+	config = function()
+		require("neogit").setup({
+			graph_style = "kitty",
+		})
+	end
 }
