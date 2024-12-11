022cd0bae46b908c29832f5a1efcee88e7686e35
Lento Manickathan <lento.manickathan@gmail.com>
2024-12-11 15:17:35 +0100

    added snacks nomnomnom

---
+++ .config/nvim/lua/custom/plugins/snacks.lua
@@ -0,0 +1,33 @@
+return {
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+		bigfile = { enabled = true },
+		indent = { enabled = true },
+		input = { enabled = true },
+		git = { enabled = true },
+		lazygit = { enabled = true },
+		notifier = { enabled = true },
+		quickfile = { enabled = true },
+		scroll = { enabled = true },
+		statuscolumn = { enabled = true },
+		terminal = { enabled = true },
+		words = {
+			debounce = 0, -- time in ms to wait before updating
+			-- enabled = true
+		},
+	},
+	keys = {
+		{ "<leader>z",  function() Snacks.zen() end,                     desc = "Toggle Zen Mode" },
+		{ "<leader>gB", function() Snacks.gitbrowse() end,               desc = "Git Browse" },
+		{ "<leader>gb", function() Snacks.git.blame_line() end,          desc = "Git Blame Line" },
+		{ "<leader>lg", function() Snacks.lazygit() end,                 desc = "Lazygit" },
+		{ "<leader>gl", function() Snacks.lazygit.log() end,             desc = "Lazygit Log (cwd)" },
+		{ "<leader>k",  function() Snacks.words.jump(vim.v.count1) end,  desc = "Next Reference",   mode = { "n", "t" } },
+		{ "<leader>j",  function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference",   mode = { "n", "t" } },
+	},
+}
