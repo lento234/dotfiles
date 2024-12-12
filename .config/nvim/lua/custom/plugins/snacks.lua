2045657d8dde16ee85ceee2ab16af7abc5e3778d
Lento Manickathan <lento.manickathan@gmail.com>
2024-12-12 10:52:04 +0100

    less annoying

--- .config/nvim/lua/custom/plugins/snacks.lua
+++ .config/nvim/lua/custom/plugins/snacks.lua
@@ -2,25 +2,6 @@ return {
 	"folke/snacks.nvim",
 	priority = 1000,
 	lazy = false,
-	opts = {
-		-- your configuration comes here
-		-- or leave it empty to use the default settings
-		-- refer to the configuration section below
-		bigfile = { enabled = true },
-		indent = { enabled = true },
-		input = { enabled = true },
-		git = { enabled = true },
-		lazygit = { enabled = true },
-		notifier = { enabled = true },
-		quickfile = { enabled = true },
-		scroll = { enabled = true },
-		statuscolumn = { enabled = true },
-		terminal = { enabled = true },
-		words = {
-			debounce = 0, -- time in ms to wait before updating
-			-- enabled = true
-		},
-	},
 	keys = {
 		{ "<leader>z",  function() Snacks.zen() end,                     desc = "Toggle Zen Mode" },
 		{ "<leader>gB", function() Snacks.gitbrowse() end,               desc = "Git Browse" },
@@ -30,4 +11,29 @@ return {
 		{ "<leader>k",  function() Snacks.words.jump(vim.v.count1) end,  desc = "Next Reference",   mode = { "n", "t" } },
 		{ "<leader>j",  function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference",   mode = { "n", "t" } },
 	},
+	config = function()
+		require("snacks").setup(
+			{
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+				bigfile = { enabled = true },
+				indent = {
+					enabled = false, -- enable indent guides
+				},
+				input = { enabled = true },
+				git = { enabled = true },
+				lazygit = { enabled = true },
+				notifier = { enabled = false },
+				quickfile = { enabled = true },
+				scroll = { enabled = true },
+				statuscolumn = { enabled = true },
+				-- terminal = { enabled = true },
+				words = {
+					debounce = 200, -- time in ms to wait before updating
+					-- enabled = true
+				},
+			}
+		)
+	end
 }
