34b21635c535fd44273f4f8ebff3eccc37e4c9e7
Lento Manickathan <lento.manickathan@gmail.com>
2024-12-13 09:26:10 +0100

    added dropbar and overseer

---
+++ .config/nvim/lua/custom/plugins/overseer.lua
@@ -0,0 +1,8 @@
+return {
+	'stevearc/overseer.nvim',
+	opts = {},
+	keys = {
+		{ "<leader>or", ":OverseerRun<CR>",    desc = "[o]verseer [r]un",    silent = true },
+		{ "<leader>ot", ":OverseerToggle<CR>", desc = "[o]verseer [t]oggle", silent = true },
+	},
+}
