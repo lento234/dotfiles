23add55a4e62d557e8c7b80317cc3bc8d25351ef
Lento Manickathan <lento.manickathan@gmail.com>
2025-09-09 18:12:04 +0200

    linted

--- .config/nvim/lua/custom/plugins/which-key.lua
+++ .config/nvim/lua/custom/plugins/which-key.lua
@@ -1,19 +1,19 @@
 return {
-	"folke/which-key.nvim",
-	event = "VeryLazy",
-	init = function()
-		vim.o.timeout = true
-		vim.o.timeoutlen = 300
-	end,
-	opts = {
-		spec = {
-			{ "<leader>h", group = "Git [H]unk", mode = { "n", "v" } },
-			{ "<leader>f", group = "[F]ind" },
-			{ "<leader>d", group = "[D]ap" },
-			{ "<leader>x", group = "Trouble" },
-			{ "<leader>t", group = "Toggle" },
-			{ "<leader>o", group = "Obsidian" },
-			{ "<leader>r", group = "Overseer" },
-		},
-	},
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  init = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 300
+  end,
+  opts = {
+    spec = {
+      { "<leader>h", group = "Git [H]unk", mode = { "n", "v" } },
+      { "<leader>f", group = "[F]ind" },
+      { "<leader>d", group = "[D]ap" },
+      { "<leader>x", group = "Trouble" },
+      { "<leader>t", group = "Toggle" },
+      { "<leader>o", group = "Obsidian" },
+      { "<leader>r", group = "Overseer" },
+    },
+  },
 }
