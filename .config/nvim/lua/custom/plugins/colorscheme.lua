d1075867f5499f135d664baae2525b7ca324aa04
Lento Manickathan <lento.manickathan@gmail.com>
2026-08-17 11:46:36 +0200

    updated

--- .config/nvim/lua/custom/plugins/colorscheme.lua
+++ .config/nvim/lua/custom/plugins/colorscheme.lua
@@ -1,27 +1,36 @@
+-- return {
+-- 	"ellisonleao/gruvbox.nvim",
+-- 	lazy = false,
+-- 	priority = 1000,
+-- 	config = function()
+-- 		-- load the colorscheme here
+-- 		require("gruvbox").setup({
+-- 			contrast = "hard",
+-- 			overrides = {
+-- 				SignColumn = { bg = "#1d2021" },
+-- 				TabLineFill = { bg = "#1d2021" },
+-- 				TabLineSel = { bg = "#1d2021" },
+-- 				CursorLine = { bg = "#282828" },
+-- 				-- nvim-tree
+-- 				NvimTreeFolderIcon = { link = "GruvboxBlueBold" },
+-- 				NvimTreeFolderName = { link = "GruvboxBlue" },
+-- 				NvimTreeOpenedFolderName = { link = "GruvboxBlueBold" },
+-- 				NvimTreeEmptyFolderName = { link = "GruvboxBlue" },
+-- 				-- neo-tree
+-- 				NeoTreeDirectoryIcon = { link = "GruvboxBlueBold" },
+-- 				NeoTreeDirectoryName = { link = "GruvboxBlue" },
+-- 				NeoTreeRootName = { link = "GruvboxPurpleBold" },
+-- 			},
+-- 		})
+-- 		vim.cmd("colorscheme gruvbox")
+-- 	end,
+-- }
+-- lua/plugins/rose-pine.lua
+
 return {
-	"ellisonleao/gruvbox.nvim",
-	lazy = false,
-	priority = 1000,
+	"rose-pine/neovim",
+	name = "rose-pine",
 	config = function()
-		-- load the colorscheme here
-		require("gruvbox").setup({
-			contrast = "hard",
-			overrides = {
-				SignColumn = { bg = "#1d2021" },
-				TabLineFill = { bg = "#1d2021" },
-				TabLineSel = { bg = "#1d2021" },
-				CursorLine = { bg = "#282828" },
-				-- nvim-tree
-				NvimTreeFolderIcon = { link = "GruvboxBlueBold" },
-				NvimTreeFolderName = { link = "GruvboxBlue" },
-				NvimTreeOpenedFolderName = { link = "GruvboxBlueBold" },
-				NvimTreeEmptyFolderName = { link = "GruvboxBlue" },
-				-- neo-tree
-				NeoTreeDirectoryIcon = { link = "GruvboxBlueBold" },
-				NeoTreeDirectoryName = { link = "GruvboxBlue" },
-				NeoTreeRootName = { link = "GruvboxPurpleBold" },
-			},
-		})
-		vim.cmd("colorscheme gruvbox")
+		vim.cmd("colorscheme rose-pine-moon")
 	end,
 }
