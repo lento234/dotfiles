e8de5d392d0fe8dc5109754a76fc5e41ce0adc2b
Lento Manickathan <lento.manickathan@gmail.com>
2026-08-26 10:58:18 +0200

    updated to latest theme

--- .config/nvim/lua/custom/plugins/nvim-colorizer.lua
+++ .config/nvim/lua/custom/plugins/nvim-colorizer.lua
@@ -1,15 +1,9 @@
 return {
-	"norcalli/nvim-colorizer.lua",
-	event = "VeryLazy",
-	config = function()
-		require("colorizer").setup({
-			"css",
-			"javascript",
-			"vue",
-			"typescript",
-			"html",
-		})
-	end,
+	"catgoose/nvim-colorizer.lua",
+	event = "BufReadPre",
+	opts = {
+		options = { parsers = { css = true, names = false } },
+	},
 	keys = {
 		{ "<leader>tc", ":ColorizerToggle<CR>", desc = "[t]oggle [c]olorized" },
 	},
