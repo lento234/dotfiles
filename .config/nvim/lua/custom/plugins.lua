583d6d09b7630275c8d871246b29033b1160b92c
Lento Manickathan <lento.manickathan@gmail.com>
2026-08-28 09:57:33 +0200

    added linting

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -54,7 +54,7 @@ vim.cmd.colorscheme("catppuccin-mocha")
 vim.pack.add({ gh("nvim-mini/mini.nvim") })
 if vim.g.have_nerd_font then
 	require("mini.icons").setup({})
-	MiniIcons.mock_nvim_web_devicons({})
+	MiniIcons.mock_nvim_web_devicons()
 end
 
 -- Better Around/Inside textobjects
@@ -486,6 +486,61 @@ vim.api.nvim_create_autocmd("FileType", {
 		else
 			-- Try to enable treesitter features in case the parser exists but is not available from `nvim-treesitter`
 			treesitter_try_attach(buf, language)
+		end
+	end,
+})
+
+-- linting
+vim.pack.add({ gh("mfussenegger/nvim-lint") })
+
+local lint = require("lint")
+lint.linters_by_ft = {
+	markdown = { "markdownlint" }, -- Make sure to install `markdownlint` via mason / npm
+	json = { "jsonlint" },
+}
+
+-- To allow other plugins to add linters to require('lint').linters_by_ft,
+-- instead set linters_by_ft like this:
+-- lint.linters_by_ft = lint.linters_by_ft or {}
+-- lint.linters_by_ft['markdown'] = { 'markdownlint' }
+--
+-- However, note that this will enable a set of default linters,
+-- which will cause errors unless these tools are available:
+-- {
+--   clojure = { "clj-kondo" },
+--   dockerfile = { "hadolint" },
+--   inko = { "inko" },
+--   janet = { "janet" },
+--   markdown = { "vale" },
+--   rst = { "vale" },
+--   ruby = { "ruby" },
+--   terraform = { "tflint" },
+--   text = { "vale" }
+-- }
+--
+-- You can disable the default linters by setting their filetypes to nil:
+-- lint.linters_by_ft['clojure'] = nil
+-- lint.linters_by_ft['dockerfile'] = nil
+-- lint.linters_by_ft['inko'] = nil
+-- lint.linters_by_ft['janet'] = nil
+-- lint.linters_by_ft['json'] = nil
+-- lint.linters_by_ft['markdown'] = nil
+-- lint.linters_by_ft['rst'] = nil
+-- lint.linters_by_ft['ruby'] = nil
+-- lint.linters_by_ft['terraform'] = nil
+-- lint.linters_by_ft['text'] = nil
+
+-- Create autocommand which carries out the actual linting
+-- on the specified events.
+local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+	group = lint_augroup,
+	callback = function()
+		-- Only run the linter in buffers that you can modify in order to
+		-- avoid superfluous noise, notably within the handy LSP pop-ups that
+		-- describe the hovered symbol using Markdown.
+		if vim.bo.modifiable then
+			lint.try_lint()
 		end
 	end,
 })
